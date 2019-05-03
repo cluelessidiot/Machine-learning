431cb37bb2f07cb92dab3ec8d67732becbb23e7b
cluelessidiot <aathiltashraf@gmail.com>
2019-05-03 11:22:10 +0530

    dictionary

---
+++ python basics/dict.py
@@ -0,0 +1,12 @@
+def dictionary():
+	print("1")
+	ab={'a':'z','b':'x','c':'y','aathil':'nasi'}
+	print("aathil is ",ab['aathil'])
+	print("there are {} elements in dictionary and {} crosses to {}".format(len(ab),"aathil",ab['aathil']))
+	print(ab.keys())
+	l=list(ab.keys())
+	#listing keys......
+	for i in range(len(l)):
+	   print(l[i])
+if __name__=='__main__':
+	dictionary()	
