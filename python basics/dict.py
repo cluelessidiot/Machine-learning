85e5092dd66f152333a912eed719ff345953dfa7
cluelessidiot <aathiltashraf@gmail.com>
2019-05-03 15:36:42 +0530

    modified

--- python basics/dict.py
+++ python basics/dict.py
@@ -8,5 +8,9 @@ def dictionary():
 	#listing keys......
 	for i in range(len(l)):
 	   print(l[i])
+	if 'aathil'not in l:
+	   print('yaaay')
+	q=set(['aathil','3'])
+	print(l& q)      
 if __name__=='__main__':
 	dictionary()	
